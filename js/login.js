3026e7c6926a7ba174e4d757a0dad61368e3578d
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-07-07 15:37:30 -0300

    criado js para fazer login

---
+++ js/login.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const formLogin = document.getElementById("formLogin");
+
+    formLogin.addEventListener("submit", (event) => {
+
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
+        const usuarioSalvo = localStorage.getItem("usuarioCadastrado");
+
+        if (!usuarioSalvo) {
+            alert("Nenhum usuário cadastrado. Crie uma conta primeiro!");
+            window.location.href = "cadastro.html";
+            return;
+        }
+
+        const usuario = JSON.parse(usuarioSalvo);
+
+        if (email === usuario.email && senha === usuario.senha) {
+            alert("Login realizado com sucesso!");
+            localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+            window.location.href = "../index.html";
+        } else {
+            alert("Email ou senha incorretos!");
+        }
+    });
+});
